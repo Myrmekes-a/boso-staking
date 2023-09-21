62c7cc70937bf42a8a7c26fa32c20962f2ec5c30
Myrmekes-a <nightfurry090@gmail.com>
2023-09-21 16:00:58 +0200

    removed central img

--- frontend/components/sections/Hero.tsx
+++ frontend/components/sections/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
         alt="Bozo"
         className="anim-perspective mt-[180px] hero-logo sm:mt-[80px] md:mt-[150px] lg:mt-[100px] xl:mt-[50px] 2xl:mt-[0px] max-w-[95%] md:max-w-[85%] lg:max-w-[70%] mx-auto pointer-events-none"
       />
-      <div className="absolute bottom-0 w-full h-screen  pointer-events-none hero-img-container">
+      <div className="absolute bottom-0 w-full h-screen overflow-hidden pointer-events-none hero-img-container">
         <Image
           src="/img/hero/left-hero.png"
           width={1000}
@@ -21,13 +21,13 @@ const Hero = () => {
           alt="Bozo"
           className="absolute left-0 bottom-0 md:w-[60%] lg:w-2/5 z-[3] hidden md:block md:scale-100 2xl:bottom-[-2rem] hero-img-1"
         />
-        <Image
+        {/* <Image
           src="/img/hero/center-hero.png"
           width={1000}
           height={1000}
           alt="Bozo"
           className="absolute lg:left-0 md:right-[-2rem] lg:right-0 bottom-0 lg:mx-auto w-full lg:w-[45%] md:w-[60%] z-[2] scale-150 md:scale-100 origin-bottom 2xl:bottom-[-2rem] hero-img-2"
-        />
+        /> */}
         <Image
           src="/img/hero/right-hero.png"
           width={1000}
