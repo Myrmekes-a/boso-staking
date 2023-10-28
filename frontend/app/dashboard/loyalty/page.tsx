c4e1a17e02d81099d6801bf8d89b75c0aeab6f90
Myrmekes-a <nightfurry090@gmail.com>
2023-10-28 20:36:30 +0200

    fix

--- frontend/app/dashboard/loyalty/page.tsx
+++ frontend/app/dashboard/loyalty/page.tsx
@@ -166,6 +166,7 @@ export default function Loyalty() {
         );
         setStakedNfts(tmpStaked);
         setNfts(tmpNfts);
+        console.log(error);
       } finally {
         timerRef.current = setTimeout(() => {
           setShouldFetch(true);
