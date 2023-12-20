60d45f2c322b249ff14af38ef950f6173c61a038
Myrmekes-a <nightfurry090@gmail.com>
2023-12-21 02:50:15 +0900

    fix: filter pools only has stakings

--- force-unstake/src/checkStakeEntries.ts
+++ force-unstake/src/checkStakeEntries.ts
@@ -5,7 +5,9 @@ import { getProgramIdlAccounts } from './sdk';
 export const checkStakeEntries = async (connection: Connection) => {
   const programAccounts = await getProgramIdlAccounts(connection, 'stakeEntry');
   const nulledAccounts = programAccounts.filter((acc) => !acc.parsed);
-  const entries = programAccounts.filter((acc) => !!acc.parsed);
+  const entries = programAccounts.filter(
+    (acc) => acc.parsed?.amount.toNumber() === 1
+  );
   console.log(`Found ${entries.length} pools`);
   console.log(
     `[success] [${nulledAccounts.length}]`,
